d7e1360fbe8f9c906cf7237a692ef00960fc3326
NickRoss <nick.jp.ros@gmail.com>
2020-05-15 06:24:00 -0600

    Initial commit

---
+++ Utilities/general_logs.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+def setup_general_logger(log_name):
+    # create and configure main logger
+    logger = logging.getLogger(log_name)
+    logger.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    screen_handler = logging.StreamHandler()
+    screen_handler.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(f'exercises.log', 'a')
+
+    # create formatter and add it to the handler
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(formatter)
+    screen_handler.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(screen_handler)
+
+    return logger
---
+++ scripts.py
@@ -0,0 +1,7 @@
+import Utilities.general_logs as logs
+
+log = logs.setup_general_logger(__file__)
+
+
+if __name__ == '__main__':
+    log.debug('test')
